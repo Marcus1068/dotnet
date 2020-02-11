d06ff35b0b2d2107ff373625872f5c1e8dc1f7f9
Marcus Deuss <marcus.deuss@me.com>
2020-02-11 22:43:59 +0100

    initial

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace dotnet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!");
+
+            var x = 0;
+            var y = 0;
+
+            x = y;
+        }
+    }
+}
